10b3e65aa5c1283f7a11c61e3ad0cbdc6153cac6
Philip Rader <pjrader1@yahoo.com>
2017-03-26 00:30:31 -0700

    Added Entitas library (ECS)
    https://github.com/sschmid/Entitas-CSharp

---
+++ SuperPong/SuperPong/GameState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.Xna.Framework.Content;
+
+namespace SuperPong
+{
+	public abstract class GameState
+	{
+		protected ContentManager content;
+
+		public GameState()
+		{
+		}
+
+		public abstract void LoadContent();
+
+		public abstract void UnloadContent();
+	}
+}
