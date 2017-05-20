784d9b52ab079abf376939f3a2721e32ca4297c4
Philip Rader <pjrader1@yahoo.com>
2017-05-19 20:24:40 -0700

    Added gamepad input support

---
+++ SuperPong/SuperPong/Constants/Input.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace SuperPong.Constants
+{
+    public class Input
+    {
+        public static readonly float DEADZONE = 0.1f;
+    }
+}
---
+++ SuperPong/SuperPong/Input/ControllerInputMethod.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using SuperPong.Common;
+
+namespace SuperPong.Input
+{
+    public class ControllerInputMethod : InputMethod
+    {
+        readonly PlayerIndex _playerIndex;
+        readonly GamePadCapabilities _capabilities;
+
+        public ControllerInputMethod(PlayerIndex playerIndex)
+        {
+            _playerIndex = playerIndex;
+            _capabilities = GamePad.GetCapabilities(_playerIndex);
+        }
+
+        public override void Update(float dt)
+        {
+            if (_capabilities.IsConnected)
+            {
+                GamePadState currentState = GamePad.GetState(_playerIndex);
+
+                float axis = currentState.ThumbSticks.Left.Y;
+                axis = MathUtils.Clamp(-1, 1, axis);
+
+                if (Math.Abs(axis) < Constants.Input.DEADZONE)
+                {
+                    axis = 0;
+                }
+
+                _snapshot._axis = axis;
+            }
+        }
+    }
+}
