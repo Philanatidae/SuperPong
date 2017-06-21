bfefc1b4c0cb7d6e4bb9b04c36e953528b62cb9d
Philip Rader <pjrader1@yahoo.com>
2017-06-20 22:16:17 -0700

    Added a widget for rendering nine patches

---
+++ SuperPong/SuperPong/UI/Widgets/NinePatchImage.cs
@@ -0,0 +1,62 @@
+﻿using Events;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.TextureAtlases;
+
+namespace SuperPong.UI.Widgets
+{
+    public class NinePatchImage : Widget
+    {
+        readonly NinePatchRegion2D _ninePatch;
+
+        public NinePatchImage(NinePatchRegion2D ninePatch,
+                      Origin origin,
+                      float percentX,
+                      float pOffsetX,
+                      float percentY,
+                      float pOffsetY,
+                      float percentAspect,
+                      float pOffsetAspect,
+                      float aspectRatio,
+                      AspectRatioType aspectRatioType)
+            : base(origin, percentX, pOffsetX, percentY, pOffsetY,
+                   percentAspect, pOffsetAspect, aspectRatio, aspectRatioType)
+        {
+            _ninePatch = ninePatch;
+        }
+
+        public NinePatchImage(NinePatchRegion2D ninePatch,
+                      Origin origin,
+                      float percentX,
+                      float pOffsetX,
+                      float percentY,
+                      float pOffsetY,
+                      float percentWidth,
+                      float pOffsetWidth,
+                      float percentHeight,
+                      float pOffsetHeight)
+            : base(origin, percentX, pOffsetX, percentY, pOffsetY,
+                 percentWidth, pOffsetWidth, percentHeight, pOffsetHeight)
+        {
+            _ninePatch = ninePatch;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            if (!Hidden)
+            {
+                spriteBatch.Draw(_ninePatch,
+                             new Rectangle((int)TopLeft.X,
+                                           (int)TopLeft.Y,
+                                           (int)Width,
+                                           (int)Height),
+                             Color.White);
+            }
+        }
+
+        public override bool Handle(IEvent evt)
+        {
+            return false;
+        }
+    }
+}
